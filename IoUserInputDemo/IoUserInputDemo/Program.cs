1c4354148e671cb5ef7070d99c8fd0d367be7289
Nishigandha.Patel <nishigandha.patel@zensar.com>
2021-01-27 22:48:48 +0530

    First commit

---
+++ IoUserInputDemo/IoUserInputDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Text;
+
+namespace IoUserInputDemo
+{
+    public class Program
+    {
+
+            static void WriteIntoFile()
+            {
+                try
+                {
+                    using (FileStream fstream = new FileStream(@"F:\DotNetBatch2021\ConsoleAssignments\ConsoleAssignment-25-01-2021\file.txt", FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        using (StreamWriter srr = new StreamWriter(fstream))
+                        {
+                            Console.WriteLine("Enter the String to write in file :");
+                            string str = null;
+                            while (str != "&")
+                            {
+                                str = Console.ReadLine();
+                                if (str != "&")
+                                {
+                                    srr.WriteLine(str);
+                                }
+                            }
+
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            static void ReadFromFile()
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(@"D:\DotNETBatch2021\Assignments\ConsoleAssignment25Jan2021\ConFileInputOutput\ConFileInputOutput\file.txt"))
+                    {
+
+                        Console.WriteLine("Reading contents of input.txt : \n" + sr.ReadToEnd());
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            static void Main(string[] args)
+            {
+
+                Program.WriteIntoFile();
+                Program.ReadFromFile();
+
+            }
+        }
+    }
---
+++ VariousIoClasses/VariousIoClasses/BinaryCL.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariousIoClasses
+{
+
+    //C# BinaryReader Object works at lower level of Streams. 
+    // C# BinaryReader is using for read primitive types as binary values in a specific encoding stream. Binaryreader Object works with Stream Objects that provide access to the underlying bytes. For creating a BinaryReader Object , 
+    //you have to first create a FileStream Object and then pass BinaryReader to the constructor method .
+    //he main advantages of Binary information is that 
+    //stores files as Binary format is the best practice of space utilization.
+    class BinaryCL
+    {
+        static void Main()
+        {
+
+            BinaryCL b = new BinaryCL();
+            b.Writer();
+            b.Reader();
+
+        }
+        public void Writer()
+        {
+            try
+            {
+
+                FileStream fs = new FileStream(@"F:\DotNetBatch2021\ConsoleAssignments\ConsoleAssignment-25-01-2021\Binary.txt", FileMode.OpenOrCreate,
+                FileAccess.Write, FileShare.ReadWrite);
+
+                Console.WriteLine("We are writing in Binary.txt using stream BinaryWriter");
+                BinaryWriter bw = new BinaryWriter(fs);
+
+
+                string name = "Nishi";
+                int age = 22;
+                double height = 5.2;
+                char gender = 'F';
+
+
+                bw.Write(name);
+                bw.Write(age);
+                bw.Write(height);
+                bw.Write(gender);
+
+                bw.Close();
+                Console.WriteLine();
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private void Reader()
+        {
+            try
+            {
+
+                FileStream fin = new FileStream(@"F:\DotNetBatch2021\ConsoleAssignments\ConsoleAssignment-25-01-2021\Binary.txt", FileMode.Open,
+                FileAccess.Read, FileShare.ReadWrite);
+                Console.WriteLine("We are Reading from BInary.txt using stream BinaryReader");
+
+                BinaryReader br = new BinaryReader(fin);
+                br.BaseStream.Seek(0, SeekOrigin.Begin);
+
+                string name = br.ReadString();
+                int age = br.ReadInt32();
+                double height = br.ReadDouble();
+                char gender = br.ReadChar();
+
+                Console.WriteLine("Name :" + name);
+                Console.WriteLine("Age :" + age);
+                Console.WriteLine("Height :" + height);
+                Console.WriteLine("Gender M/F:" + gender);
+
+
+                br.Close();
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
+
